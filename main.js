2bbc654fa2cb0ee3095af09ac22e78e49eb88eba
Night6997 <83875915+Night6997@users.noreply.github.com>
2021-05-10 12:23:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function ChangeImage1() {
+    document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg";
+}
+
+function ChangeImage2() {
+    document.getElementById("img2").src = "https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954__340.jpg";
+}
+
+function ChangeImage3() {
+    document.getElementById("img3").src = "https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567__340.jpg";
+}
+
+function ChangeImage4() {
+    document.getElementById("img4").src = "https://cdn.pixabay.com/photo/2016/10/22/17/46/mountains-1761292__340.jpg";
+}
+
+function setDate() {
+    document.getElementById("date").innerHTML = new Date().toDateString();
+}
